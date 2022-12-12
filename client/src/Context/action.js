cbac169492fc75886cdb1cfd6d77be9d8e29b1b8
Ethan Su <hinsssssssss@gmail.com>
2022-12-12 16:07:08 +1300

    first commit

--- client/src/Context/action.js
+++
@@ -1,3 +0,0 @@
-export const DISPLAY_ALERT = 'SHOW_ALERT'
-export const CLEAR_ALERT = "CLEAR_ALERT"
-
--- middleware/not-found.js
+++
@@ -1,3 +0,0 @@
-const notFoundMiddleware = (req,res) => res.status(404).send("Route not found")
-
-export default notFoundMiddleware
